1f92787d461548dd210e514435a6b51ddbd2a972
Sviatoslav Samoilov <worldinfair761@gmail.com>
2023-10-25 19:29:12 +0400

    Project creation.

---
+++ MathTrainer/MathTrainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathTrainer
+//
+//  Created by Sviatoslav Samoilov on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
